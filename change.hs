f4320286cb6906ccc2ba7bc5246d6671851e929e
jjfine <jjfine@gmail.com>
2014-01-29 16:24:01 +0800

    factor out counting distinct solutions

--- change.hs
+++ change.hs
@@ -6,7 +6,7 @@ import Data.Set
 data Tree a = Root [Tree a] | Node a [Tree a] deriving (Show, Read, Eq) 
 
 infiniteTree :: [Int] -> Tree Int
-infiniteTree values = Root [treeBranch values branchValue | branchValue <- values]
+infiniteTree values = Root [treeBranch values nextBranchValue | nextBranchValue <- values]
 
 treeBranch :: [Int] -> Int -> Tree Int
 treeBranch values branchValue = Node branchValue [treeBranch values nextBranchValue | nextBranchValue <- values]
@@ -15,10 +15,14 @@ treeBranch values branchValue = Node branchValue [treeBranch values nextBranchVa
 findTotal ::  Int -> [Int] -> Tree Int -> [[Int]]
 findTotal targetValue _ (Root branches) = concatMap (findTotal targetValue []) branches
 findTotal targetValue pattern (Node branchValue branches)
-    | branchValue + (sum pattern) == targetValue = [ sort (pattern ++ [branchValue]) ]
+    | branchValue + (sum pattern) == targetValue = [ pattern ++ [branchValue] ]
     | branchValue + (sum pattern) > targetValue = []
     | branchValue + (sum pattern) < targetValue = concatMap (findTotal targetValue (pattern++[branchValue])) branches
 
+countDistinct :: [[Int]] -> Int
+countDistinct = size . fromList . Data.List.map sort
+
 main :: IO ()
 main = do
-    print $ size . fromList . findTotal 4 [] $ infiniteTree [1,2,3]
+    print $ countDistinct . findTotal 4 [] $ infiniteTree [1,2,3]
+    

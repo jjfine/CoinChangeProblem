c1dc54767e25a7e2587f0d00ad5702b86d52a650
Jeremy Fine <jjfine@gmail.com>
2014-01-30 14:46:38 +0800

    finally faster...i think...

--- change.hs
+++ change.hs
@@ -3,21 +3,22 @@ module Main where
 import Data.List
 import Data.Array
 import Debug.Trace
-import Data.Set
+import qualified Data.Set as Set
 
 combos :: [Int] -> Int -> Int
 combos denominations target = arr ! target
   where
-      arr = listArray (0, target) (1: Data.List.map m [1..target])
-      m total = sum [  arr ! (total - value) | value <- denominations, value <= total]
+    arr = listArray (0, target) (1: Data.List.map m [1..target])
+    m total = sum [  arr ! (total - value) | value <- denominations, value <= total]
 
-comboPatterns :: [Int] -> Int -> [[Int]]
+comboPatterns :: [Int] -> Int -> Set.Set [Int]
 comboPatterns denominations target = arr ! target
   where
-      arr = listArray (0, target) (([[]] :: [[Int]]) : Data.List.map m [1..target])
-      m total = concat [ Data.List.map ((:) value) (arr ! (total - value)) | value <- denominations, value <= total]
+    arr = listArray (0, target) ((Set.singleton [] :: Set.Set [Int]) : Data.List.map m [1..target])
+    m total = Set.unions [ Set.map (sort . (:) value) (arr ! (total - value)) | value <- denominations, value <= total]
       
 main = do
-    denominations <- getLine
-    target <- getLine
-    print $ Data.Set.size . fromList $ Data.List.map sort $ comboPatterns (read ( "[" ++ denominations ++ "]" ))  (read target)
+  denominations <- getLine
+  target <- getLine
+  print $ Set.size $ comboPatterns (read ("[" ++ denominations ++ "]")) (read target)
+

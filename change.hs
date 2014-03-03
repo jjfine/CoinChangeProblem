aea4fb260fdd8aa68ca90ca62d996beb6b795153
Jeremy Fine <jjfine@gmail.com>
2014-03-03 21:10:48 +0800

    cheated and looked up the algorithm...

--- change.hs
+++ change.hs
@@ -10,20 +10,13 @@ combos denominations target = snd $ arr ! (L.length denominations, target)
   where
     arr = listArray ((0,0), ((L.length denominations), target)) [ ((denominationCount, currentTarget), totalCombos denominationCount currentTarget) | denominationCount <- [0..(L.length denominations)], currentTarget <- [0..target]]
     totalCombos 0 0 = 1
-    totalCombos 0 _ = 0
     totalCombos _ 0 = 1
-    totalCombos denominationCount currentTarget = sum [ snd (arr ! (denominationCount - 1, remaining)) | remaining <- diffList currentTarget (take denominationCount denominations)]
-    
+    totalCombos 0 _ = 0
+    totalCombos denominationCount currentTarget = (retrieveCombos (denominationCount - 1) currentTarget) + (retrieveCombos denominationCount (currentTarget - (denominations !! (denominationCount - 1))))
+    retrieveCombos denominationCount remaining  
+            | remaining < 0 = 0
+            | remaining >= 0 = snd (arr ! (denominationCount, remaining))
 
-diffList :: Int -> [Int] -> [Int]
-diffList start denominations = [ start - value | value <- denominations, value <= start]
-
-comboPatterns :: [Int] -> Int -> Set.Set [Int]
-comboPatterns denominations target = arr ! target
-  where
-    arr = listArray (0, target) ((Set.singleton [] :: Set.Set [Int]) : L.map m [1..target])
-    m total = Set.unions [ Set.map (sort . (:) value) (arr ! (total - value)) | value <- denominations, value <= total]
-      
 main = do
   denominations <- getLine
   target <- getLine
